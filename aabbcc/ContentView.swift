afa61b466359ebc666cf21d1837c1dff8202a2ef
awler1122 <asd@asd.asd>
2025-03-18 21:01:23 -0700

    Initial Commit

---
+++ aabbcc/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  aabbcc
+//
+//  Created by Os on 28/12/1403 AP.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ aabbcc/aabbccApp.swift
@@ -0,0 +1,17 @@
+//
+//  aabbccApp.swift
+//  aabbcc
+//
+//  Created by Os on 28/12/1403 AP.
+//
+
+import SwiftUI
+
+@main
+struct aabbccApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
